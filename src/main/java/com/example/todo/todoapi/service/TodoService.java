361bfe3a6bd6fdce982463124115d76bc2ecc37c
JunHyuk <kkjjhh374@naver.com>
2023-11-24 10:25:16 +0900

    #1. todo api 할 일 기능 작성 중(create, select)

--- src/main/java/com/example/todo/todoapi/service/TodoService.java
+++ src/main/java/com/example/todo/todoapi/service/TodoService.java
@@ -1,14 +1,20 @@
 package com.example.todo.todoapi.service;
 
+import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
+import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
+import com.example.todo.todoapi.entity.Todo;
 import com.example.todo.todoapi.repository.TodoRepository;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter @Setter
 @ToString @EqualsAndHashCode
-@AllArgsConstructor
 @Builder
 @Service
 @Slf4j
@@ -17,4 +23,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class TodoService {
 
     private final TodoRepository todoRepository;
+
+    public TodoListResponseDTO create(final TodoCreateRequestDTO requestDTO)
+            throws RuntimeException {
+        todoRepository.save(requestDTO.toEntity());
+        log.info("할 일 저장 완료! 제목: {}", requestDTO.getTitle());
+
+        return retrieve();
+    }
+
+    public TodoListResponseDTO retrieve() {
+        List<Todo> entityList = todoRepository.findAll();
+        List<TodoDetailResponseDTO> dtoList = entityList.stream()
+                .map(TodoDetailResponseDTO::new)
+                .collect(Collectors.toList());
+
+        return TodoListResponseDTO.builder()
+                .todos(dtoList)
+                .build();
+    }
+
+
 }
